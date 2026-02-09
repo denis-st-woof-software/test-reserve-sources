7df975911efee9c82eee9739cddf13c45c423d05
denis-st-woof-software <denis.st@woof.software>
2026-02-09 17:15:57 +0200

    Set up JSON datasets, validation, formatting, and Husky pre-commit hook

---
+++ scripts/setup-husky.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+const rootDir = path.resolve(__dirname, '..');
+const huskyDir = path.join(rootDir, '.husky');
+const hookPath = path.join(huskyDir, 'pre-commit');
+
+const hookContent = `#!/usr/bin/env sh
+npm run validate:json
+`;
+
+fs.mkdirSync(huskyDir, { recursive: true });
+
+if (!fs.existsSync(hookPath)) {
+  fs.writeFileSync(hookPath, hookContent, 'utf8');
+}
+
+fs.chmodSync(hookPath, 0o755);
